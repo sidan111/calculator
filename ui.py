ff914f9d7f00fd64372af2e985b5da0a1644b718
“developer1” <dev1@email.com>
2024-12-20 14:38:31 +0900

    Modify activateMessage function to add an argument

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-# ch 6.2.1 ui.py
+# ch 6.3.2 ui.py
 
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
@@ -52,8 +52,8 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
+    def activateMessage(self, text):
+        self.te1.appendPlainText(text)
         
     def clearMessage(self):
         self.te1.clear()
